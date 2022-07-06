c13a83b5f369fbce6082cba95c4c8feb632b29b5
Binnette <binnette@gmail.com>
2022-07-06 23:32:53 +0200

    Import ninja res

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,25 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
     })
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ponyracer'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ponyracer');
-  });
+  );
 
   it('should have a title', () => {
     const fixture = TestBed.createComponent(AppComponent);
